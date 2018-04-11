a11abf96dac3c6257fae66411a2c54b6212ad0d6
dgates8 <djgates@syr.edu>
2018-04-11 10:23:32 -0500

    Delete coin.cpp

--- blockchain/coin.cpp
+++
@@ -1,9 +0,0 @@
-#include "coin.h"
-#include <sstream>
-
-Trick::Trick() {}
-
-Trick::Trick(bool s, std::string c) : spent(s), coin(c) {}
-
-bool Trick::get_spent()             {return spent;}
-std::string Trick::get_coin()       {return coin;}
